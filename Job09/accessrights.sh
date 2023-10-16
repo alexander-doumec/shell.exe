cfae99219dd162bfb8b7be760dfaf892d26feb1e
alexander-doumec <alexander.doumec@laplateforme.io>
2023-10-16 11:13:15 +0200

    access

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,2 +1,8 @@
-cut Shell_Userlist.csv
+csv_file="Shell_Userlist.csv"
+colonne_role=$(cut -d ',' -f 4 "$csv_file")
+colonne_name=$(cut -d ',' -f 1 "$csv_file")
+
+
+echo $colonne_role
+echo $colonne_name
 
